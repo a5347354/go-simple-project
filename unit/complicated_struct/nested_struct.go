c9a135b3d0539e554d7010b7913eecfe7235dc90
Luke <a5347354@gmail.com>
2020-03-23 15:41:34 +0800

    Add nested_struct

---
+++ unit/complicated_struct/nested_struct.go
@@ -0,0 +1,47 @@
+package complicated_struct
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func NestedStruct() {
+	tests := []struct {
+		Host   string `json:"host"`
+		Insert bool   `json:"insert"`
+		Update bool   `json:"update"`
+		Sync   []struct {
+			ConnectionID string
+		}
+	}{
+		{
+			Host:   "192.0.2.0:8000",
+			Insert: true,
+			Update: true,
+			Sync: []struct {
+				ConnectionID string
+			}{
+				{
+					ConnectionID: "192.0.2.0:8000",
+				},
+			},
+		},
+		{
+			Host:   "192.0.2.0:80",
+			Insert: true,
+			Update: true,
+			Sync: []struct {
+				ConnectionID string
+			}{
+				{
+					ConnectionID: "192.0.2.0:80",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		fmt.Println(tt.Host)
+		foo_marshalled, _ := json.Marshal(tt)
+		fmt.Println(string(foo_marshalled))
+	}
+}
